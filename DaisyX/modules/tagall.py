81cd84ea8605c09f36cdbabc28f5cbad4b6e1438
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-14 21:07:47 +0300

    Update tagall.py

--- DaisyX/modules/tagall.py
+++ DaisyX/modules/tagall.py
@@ -13,17 +13,17 @@ from DaisyX.function.pluginhelpers import admins_only, get_text
 from DaisyX.services.pyrogram import pbot
 
 
-@pbot.on_message(filters.command("tagall") & ~filters.edited & ~filters.bot)
+@pbot.on_message(filters.command("toplan") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    await message.reply("`Processing.....`")
+    await message.reply("`İşleniyor.....`")
     sh = get_text(message)
     if not sh:
-        sh = "Hi!"
+        sh = "Selam!"
     mentions = ""
     async for member in client.iter_chat_members(message.chat.id):
         mentions += member.user.mention + " "
-    n = 4096
+    n = 10
     kk = [mentions[i : i + n] for i in range(0, len(mentions), n)]
     for i in kk:
         j = f"<b>{sh}</b> \n{i}"
