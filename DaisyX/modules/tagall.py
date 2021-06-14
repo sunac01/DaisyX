79b9f70039d8ae0f824bd5e3e33e5da42f933211
sunac01 <82957587+sunac01@users.noreply.github.com>
2021-06-14 23:21:31 +0300

    Update tagall.py

--- DaisyX/modules/tagall.py
+++ DaisyX/modules/tagall.py
@@ -16,7 +16,7 @@ from DaisyX.services.pyrogram import pbot
 @pbot.on_message(filters.command("toplan") & ~filters.edited & ~filters.bot)
 @admins_only
 async def tagall(client, message):
-    await message.reply("`Processing.....`")
+    await message.reply("`İşleniyorr.....`")
     sh = get_text(message)
     if not sh:
         sh = "Selam!"
